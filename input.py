b6b4d1d50ede232f55e30aea0e77eb6f666d76db
Akssayy47 <askhaywaghral00@gmail.com>
2025-12-30 21:52:49 +0530

    input

---
+++ input.py
@@ -0,0 +1,7 @@
+# input() = A function that prompts the user to enter data 
+#           returns the enterd data as a string
+
+name = input("enter your name ")
+city = input("enter city")
+
+print(f"Hello{name},welcome to {city} ")
